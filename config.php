d8faee8d4c90b589e9357e6056382130c7a7e213
Fahad <faa5431@g.rit.edu>
2018-08-26 23:04:05 -0400

    Update config.php

--- config.php
+++ config.php
@@ -1,15 +1,5 @@
 <?php
-/*INSERT INTO `z3r0day`.`connect` (
-`id` ,
-`title` ,
-`subject`
-)
-VALUES (
-NULL , 'helo fahad', 'how are you fahad i want meet you noe plase'
-);
- * 
- * 
- */ 
+
 require_once 'includes/class/config.class.php';
 $db = new DB('localhost','root','toor','cwas');
 
